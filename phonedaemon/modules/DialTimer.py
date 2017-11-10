cd8eb8d732fd939170fbf5de7822a646d07a8d1a
Mog <mo-g@users.noreply.github.com>
2017-11-10 17:42:36 +0000

    Fixed the dial timer and improved tests.

--- phonedaemon/modules/DialTimer.py
+++ phonedaemon/modules/DialTimer.py
@@ -13,18 +13,34 @@ class DialTimer(object):
     Reset() being called, the number is sent to the SIP handler for dialling.
     This abstraction is probably very unnecessary, but it keeps
     TelephoneDaemon() a little cleaner.
+
+    NOTE: It may be that I can rewrite this as a subclass of Timer.
+    Investigate!
     """
 
     timeout_length = 3  # Seems a sensible default to use.
     timer_object = None
-    end_of_timer_callback = None
+    timeout_callback = None
 
-    def __init__(self):
+    def __init__(self, timeout_length=None):
         """
         Set up the class.
         """
 
-        self.timer_object = Timer(self.timeout_length, self.timer_end)
+        if timeout_length:
+            self.timeout_length = timeout_length
+
+    def create(self):
+        """
+        Creates a Timer() with the specified values.
+        """
+        self.timer_object = Timer(self.timeout_length, self.timeout_callback)
+
+    def start(self):
+        """
+        Starts a new timer.
+        """
+        self.create()
         self.timer_object.start()
 
     def reset(self):
@@ -32,19 +48,12 @@ class DialTimer(object):
         Reset the timer.
         """
         self.timer_object.cancel()
-        self.__init__()
-
-    def timer_end(self):
-        """
-        Make the callback to the calling application. This is unnecessary.
-        """
-        self.end_of_timer_callback()
+        self.create()
+        self.timer_object.start()
 
     # Handles the callbacks we're supplying
-    def register_callback(self, callback, timeout_length=0):
+    def register_callback(self, callback):
         """
         Register callback for timer. This is probably also super-unnecessary.
         """
-        self.end_of_timer_callback = callback
-        if timeout_length:
-            self.timeout_length = timeout_length
+        self.timeout_callback = callback
--- phonedaemon/modules/tests/TestDialTimer.py
+++ phonedaemon/modules/tests/TestDialTimer.py
@@ -9,48 +9,85 @@ import time
 from phonedaemon.modules.DialTimer import DialTimer
 
 
-class TestDialTimer(unittest.TestCase):
+class TestNormalTimer(unittest.TestCase):
     """
-    Includes all tests for the DialTimer class.
+    Tests case where timer has been created with default timeout.
     """
     def setUp(self):
         """
         Defines variables for the test cases.
         """
-        self.defaultflag = False
-        self.timer = None
+        self.default_flag = False
+        self.reset_flag = False
+        self.default_timer = DialTimer()
+        self.reset_timer = DialTimer()
+        self.default_timer.register_callback(self.default_callback)
+        self.reset_timer.register_callback(self.reset_callback)
 
-    def callback(self):
+    def default_callback(self):
         """
         Sets a flag to True when the timer expires.
         """
-        self.defaultflag = True
+        self.default_flag = True
+
+    def reset_callback(self):
+        """
+        Sets a flag to True when the timer expires.
+        """
+        self.reset_flag = True
 
     def test_default(self):
         """
         Test the basic case: Start a timer and wait for it to expire.
         """
-        self.timer = DialTimer()
-        self.timer.register_callback(self.callback)
+        self.default_timer.start()
         time.sleep(2)
-        self.assertFalse(self.defaultflag)
+        self.assertFalse(self.default_flag)
         time.sleep(2)
-        self.assertTrue(self.defaultflag)
+        self.assertTrue(self.default_flag)
 
     def test_reset(self):
         """
         Test the common case: Start a timer, reset and wait for it to expire.
         """
-        self.timer = DialTimer()
-        self.timer.register_callback(self.callback)
+        self.reset_timer.start()
         time.sleep(2)
-        self.assertFalse(self.defaultflag)
-        self.timer.reset()
-        self.assertFalse(self.defaultflag)
+        self.assertFalse(self.reset_flag)
+        self.reset_timer.reset()
+        self.assertFalse(self.reset_flag)
         time.sleep(2)
-        self.assertFalse(self.defaultflag)
+        self.assertFalse(self.reset_flag)
         time.sleep(2)
-        self.assertTrue(self.defaultflag)
+        self.assertTrue(self.reset_flag)
+
+
+class TestLongTimer(unittest.TestCase):
+    """
+    Tests case where timer has been created with long timeout.
+    """
+    def setUp(self):
+        """
+        Defines variables for the test cases.
+        """
+        self.default_flag = False
+        self.default_timer = DialTimer(timeout_length=8)
+        self.default_timer.register_callback(self.default_callback)
+
+    def default_callback(self):
+        """
+        Sets a flag to True when the timer expires.
+        """
+        self.default_flag = True
+
+    def test_default(self):
+        """
+        Test the basic case: Start a timer and wait for it to expire.
+        """
+        self.default_timer.start()
+        time.sleep(6)
+        self.assertFalse(self.default_flag)
+        time.sleep(2)
+        self.assertTrue(self.default_flag)
 
 
 if __name__ == '__main__':
