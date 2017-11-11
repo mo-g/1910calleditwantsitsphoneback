2c1b3b7b92195177d5c43c6492e19f6dd429db49
Mog <mo-g@users.noreply.github.com>
2017-11-11 18:33:44 +0000

    Cleaned out old code from HAL

--- phonedaemon/modules/HardwareAbstractionLayer.py
+++ phonedaemon/modules/HardwareAbstractionLayer.py
@@ -6,9 +6,9 @@
 # 0 is 10 pulses
 """
 
-import time
+
 from threading import Timer
-import RPi.GPIO as GPIO
+from RPi import GPIO
 
 class HardwareAbstractionLayer(object):
     """
@@ -17,28 +17,19 @@ class HardwareAbstractionLayer(object):
     """
 
     # TODO: Why are these pins hardcoded? Should be config. So should Pi/not.
-    # We'll be reading BCM GPIO 4 (pin 7 on board)
-    pin_rotary = 4
+    pin_earpiece = 3  # on/off hook events THESE PINS ARE BCM NOT HARDWARE
+    pin_digits = 4  # rotary data source. THESE PINS ARE BCM NOT HARDWARE
+    pin_dialling = None  # this is high when the dialler is off.
 
-    # We'll be reading on/off hook events from BCM GPIO 3
-    pin_onhook = 3
+    pulse_count = 0  # Count the number of pulses detected
 
-    # After 900ms, we assume the rotation is done and we get
-    # the final digit.
-    digit_timeout = 0.9
-
-    # We keep a counter to count each pulse.
-    current_digit = 0
-
-    # Simple timer for handling the number callback
-    number_timeout = None
+    dialling_timer = None  # Timer object for dialling.
+    onhook_timer = None  # Timer object to ensure we're on hook
+    debounce_timer = None  # Timer object for debounce cleaning.
 
     last_input = 0
+    digit_timeout = 0.9  # Assume rotation is done after 900ms
 
-    # Timer to ensure we're on hook
-    onhook_timer = None
-    should_verify_hook = True
-    
     def __init__(self):
         # Set GPIO mode to Broadcom SOC numbering
         GPIO.setmode(GPIO.BCM)
@@ -47,7 +38,7 @@ class HardwareAbstractionLayer(object):
         GPIO.setup(self.pin_rotary, GPIO.IN)
         GPIO.add_event_detect(self.pin_rotary,
                               GPIO.BOTH,
-                              callback=self.click_counter)
+                              callback=self.detect_click)
 
         # Listen for on/off hooks
         GPIO.setup(self.pin_onhook, GPIO.IN)
@@ -59,44 +50,11 @@ class HardwareAbstractionLayer(object):
         self.onhook_timer = Timer(2, self.verifyHook)
         self.onhook_timer.start()
 
-    def click_counter(self, channel):
+    def detect_click(self, channel):
         """
-        Increment counter when click heard, and create digit count timeout.
+        GPIO detects a state change on the rotary detection pin. This is where
+        I count the clicks and assemble a digit from the data.
         """
-        click_input = GPIO.input(self.pin_rotary)
-        #print "[INPUT] %s (%s)" % (click_input, channel)
-        if click_input and not self.last_input:
-            self.current_digit += 1
-
-            if self.number_timeout is not None:
-                self.number_timeout.cancel()
-
-            self.number_timeout = Timer(self.digit_timeout,
-                                        self.number_detected)
-            self.number_timeout.start()
-        self.last_input = click_input
-   #     time.sleep(0.002)
-
-    def earpiece_event(self, channel):
-        """
-        Wrapper to separate on/off hook events.
-        """
-        hook_input = GPIO.input(self.pin_onhook)
-        if hook_input:
-            self.hook_state = 1
-            self.OffHookCallback()
-        else:
-            self.hook_state = 0
-            self.OnHookCallback()
-
-    def number_detected(self):
-        """
-        If a number has been detected, callback to calling application
-        """
-        if self.current_digit == 10:
-            self.current_digit = 0
-        self.NumberCallback(self.current_digit)
-        self.current_digit = 0
 
     def register_callback(self,
                           NumberCallback,
@@ -110,27 +68,6 @@ class HardwareAbstractionLayer(object):
         self.OffHookCallback = OffHookCallback
         self.OnHookCallback = OnHookCallback
         self.OnVerifyHook = OnVerifyHook
-
-        hook_input = GPIO.input(self.pin_onhook)
-        if hook_input:
-            self.OffHookCallback()
-        else:
-            self.OnHookCallback()
-
-    def StopVerifyHook(self):
-        """
-        Set state to stop verifying hook.
-        """
-        self.should_verify_hook = False
-
-    def verifyHook(self):
-        """
-        Verify whether the hook is on.
-        """
-        while self.should_verify_hook:
-            state = GPIO.input(self.pin_onhook)
-            self.OnVerifyHook(state)
-            time.sleep(1)
 
 
 class AstralHAL(HardwareAbstractionLayer):
