1e9dac48e01673606261fe65c2c3a43aa60233c4
Mog <mo-g@users.noreply.github.com>
2017-11-12 00:42:44 +0000

    Continued to add prototypes to TelephoneDaemon

--- phonedaemon/TelephoneDaemon.py
+++ phonedaemon/TelephoneDaemon.py
@@ -11,7 +11,6 @@ WebServer is also going to have some callbacks.
 """
 
 
-import os
 import Queue
 import signal
 import sys
@@ -21,9 +20,9 @@ import yaml
 from phonedaemon.modules.Ringer import AlsaRinger
 from phonedaemon.modules.HardwareAbstractionLayer \
     import HardwareAbstractionLayer
-from phonedaemon.modules.Webserver import Webserver
 from phonedaemon.modules.DialTimer import DialTimer
-from phonedaemon.modules.pjsip.SipClient import SipClient
+# from phonedaemon.modules.Webserver import Webserver
+# from phonedaemon.modules.pjsip.SipClient import SipClient
 
 
 CALLBACK_QUEUE = Queue.Queue()
@@ -34,19 +33,18 @@ class TelephoneDaemon(object):
     events.
     """
     config = None  # Contains config loaded from yaml.
-    dialling_timeout = None  # How long off-hook before tim
+    dialling_timeout = None  # How long off-hook before timeout
+    reserved_numbers = {}  # Use for implementing app-hooks, e.g. Cortana
 
     entered_digits = ""  # Stores the number to be dialled.
-
-    off_hook = False  # Flag: Is the earpiece on or off the hook?
-
-    reserved_numbers = {}
 
     app_hal = None
     app_sipclient = None
     app_webserver = None
     app_timer = None
     app_ringer = None
+
+    incoming_call = False
 
     def __init__(self):
         print "[STARTUP]"
@@ -60,7 +58,7 @@ class TelephoneDaemon(object):
         self.app_timer = DialTimer(timeout_length=self.dialling_timeout)
         # TODO: Select tone/hardware ring when latter is implemented.
         self.app_ringer = AlsaRinger(self.config["soundfiles"],
-                                 self.config["alsadevices"])
+                                     self.config["alsadevices"])
         self.app_hal = HardwareAbstractionLayer()
         signal.signal(signal.SIGINT, self.sigint_received)
         #self.app_webserver = Webserver(self)  # Currently locks thread?
@@ -90,44 +88,39 @@ class TelephoneDaemon(object):
         The user has lifted the earpiece. Start dialling.
         """
         print "[INFO] Handset lifted."
-        
-        """
+
         if self.incoming_call:
             # answer the call
-            return True
-        """
-        
-        entered_digits = ""
-        
-            # begin dialling
-            # start dialtone
-    
+            return None
+
+        self.entered_digits = ""
+        # Begin wait for dialling state.
+
     def call_number(self):
         """
         The user has entered a number. Send to SIP or handle internally.
         """
         print "[INFO] Calling number"
-        
         self.app_sipclient.SipCall(self.entered_digits)
+        # Log call to call log.
 
     def timeout_reached(self):
         """
         Check if the user has entered a number. Play the timeout tone.
         """
-        
-        if entered_digits:
+        if self.entered_digits:
             self.call_number()
-            entered_digits = ""
+            return None
         print "[INFO] Number not entered."
         self.app_ringer.play_error()
-    
+
     def digit_detected(self, digit):
         """
         The HAL has detected that a number has been dialled.
         Reset the timer and append the digit to the number to be dialled.
         """
         print "[INFO] Got digit:", digit
-        entered_digits += digit
+        self.entered_digits += digit
         self.app_timer.reset()
 
     def earpiece_replaced(self):
@@ -142,7 +135,7 @@ class TelephoneDaemon(object):
         the error code.
         """
 
-    def incoming_call(self):
+    def call_received(self):
         """
         The SIP client reports an incoming call. Cancel dialling and play the
         ringtone. This should also trap the earpiece so that the call can be
@@ -160,16 +153,22 @@ class TelephoneDaemon(object):
         The SIP client reports a dropped call.
         """
 
-    def sigint_received(self, signal, frame):
-        print "[SIGNAL] Shutting down on %s" % signal
+    def sigint_received(self, signal_name, *args):
+        """
+        Catch a signal from outside app (Ctrl-C) and shutdown cleanly.
+        """
+        print "[SIGNAL] Shutting down on:", signal_name
         self.app_hal.clean_exit()  # Not using this right now.
-        #self.app_sip_client.StopLinphone()  # Replace with pjsip clean exit
+        #self.app_sip_client.Stop()  # Replace with real function when made
         self.app_ringer.clean_exit()
         sys.exit(0)
 
 
 def main():
-    TDaemon = TelephoneDaemon()
+    """
+    Start daemon and return exit code to command l
+    """
+    return TelephoneDaemon()
 
 
 if __name__ == "__main__":
